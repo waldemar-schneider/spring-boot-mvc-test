e5e183e5546d6a9d46fe4bcf681cb183b402a875
Waldemar Schneider <schneider@louis.info>
2014-10-06 21:43:51 +0200

    removed unused import

--- src/test/java/demo/ControllerTestConfig.java
+++ src/test/java/demo/ControllerTestConfig.java
@@ -6,7 +6,6 @@ import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.web.WebAppConfiguration;
